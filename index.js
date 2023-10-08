5853320c59f79418a114743ee30a03e4ad8a11dc
Young <youngheesomemoto@gmail.com>
2023-10-08 16:36:52 -0500

    block15 done

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const userInputString = prompt(
     "Please enter some froyo flavors separated by commas.",
-    "vanilla, vanilla, vanilla, strawberry, coffee, coffee"
+    " vanilla, vanilla, vanilla, strawberry, coffee, coffee"
   );
 
-  console.table()
+//need to change prompt into from a string of string to a readable string??
+const stringArray = userInputString.split(",");
+
+//using foreach() to go through every array string and count repeats
+  const counts = [];
+  stringArray.forEach(element => {
+    counts[element] = (counts[element] || 0) + 1;
+  });
+  
+ // print as table   
+  console.table(counts)
